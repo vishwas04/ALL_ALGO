24f915c2115e191febd36474a76da17a92607ab6
vishwas <vishwasr6543@gmail.com>
2021-05-26 22:09:45 +0530

    insertion_sort

--- SORT/insertion_sort.c
+++ SORT/insertion_sort.c
@@ -1,5 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
+void insertion_sort(int * a,int len)
+{
+    int t;
+    int j;
+    for (int i = 1; i < len; i++)
+    {
+        
+        t=a[i];
+        int q=0;
+        for (j = i-1 ; j >= 0; j--)
+        {   
+            
+            if (a[j] > t)
+            {
+                q=1;
+                a[j+1]=a[j];
+            }
+            else
+                break;
+        }
+        if(q)
+            a[j+1]=t;
+        
+    }
+    
+}
 
 int main()
 {
@@ -9,7 +35,7 @@ int main()
     {
         a[i]=rand();
     }
-    bubble_sort(a,len); 
+    insertion_sort(a,len); 
     for (int i = 0; i < len; i++)
     {
         printf("%d\n",a[i]);
